ea6385895dbaa5e13cfb09ff941b7aeb7362363d
Alejandra <machate47@misena.edu.co>
2022-06-25 08:53:55 -0500

    primer commit

---
+++ Ejercicio 1.js
@@ -0,0 +1,9 @@
+class libros{
+    constructor(autor, nombre,numeropaginas){
+        this.autor=autor
+        this.nombre=nombre
+        this.numeropaginas=numeropaginas
+    }
+}
+let libro1=new libros("Gabriel Garcia marquez","cien años  de soledad","350")
+console.log(libro1)
